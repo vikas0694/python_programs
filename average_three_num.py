804dc1c02a5f190504506073083114c0d385c7b0
vikas.0694 <vikas.0694@gmail.com>
2018-11-04 12:38:34 +0530

    program to get average of 3 numbers

---
+++ average_three_num.py
@@ -0,0 +1,7 @@
+print("find average of three numbers")
+
+num1 = input("enter first num:")
+num2 = input("enter second num:")
+num3 = input("enter third num:")
+
+print(f"average of three num : {(float(num1) + float(num2) + float(num3)) / 3}")
