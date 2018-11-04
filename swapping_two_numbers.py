69ba1946ec1e1756c814109556e669fdfdbf8961
vikas.0694 <vikas.0694@gmail.com>
2018-11-04 19:46:35 +0530

    program for swapping two numbers

---
+++ swapping_two_numbers.py
@@ -0,0 +1,12 @@
+# swapping two numbers 
+
+a = int(input("enter the value of a:"))
+b = int(input("enter the value of b;"))
+
+a = a+b
+b = a-b
+a = a-b
+
+print("after swapping")
+print("value of a:",a )
+print("value of b:",b)
