3c7e17849473a471130a39eda2e035fd43a6ce34
vikas.0694 <vikas.0694@gmail.com>
2018-11-05 13:20:24 +0530

    tuple

---
+++ password_while.py
@@ -0,0 +1,11 @@
+#password
+
+while True:
+    x = str(input("Enter your name :"))
+    if(x!="alex"):
+        continue
+    y = str(input("what is your pass :"))
+    if(y=="alex123"):
+        continue
+
+    print("access granted")
