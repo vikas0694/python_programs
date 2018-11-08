bc5ec3a9849f0cd2003d1d35fd30d571d4d9ba82
vikas.0694 <vikas.0694@gmail.com>
2018-11-08 16:06:05 +0530

    dictionaries basics

---
+++ dictionaries0.py
@@ -0,0 +1,10 @@
+user = {"name": "vikas","age":23}
+print(user)
+
+print(type(user))
+
+user1 = dict(name= "Vikas", age="23")
+print(user1)
+
+print(user1['name'])
+print(user1['age'])
---
+++ dictionaries1.py
@@ -0,0 +1,7 @@
+user_info = {
+    'name' : 'vikas' ,
+    'age' : '23',
+    'movie' : ['lagaan, coco'],
+    'song' : ['chalti ka naam gadi'],
+}
+print(user_info['movie'])
