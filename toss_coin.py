e09e357d72e27da36236ae1b99ce435b906a62d7
vikas.0694 <vikas.0694@gmail.com>
2018-11-13 13:06:04 +0530

    coin toss guessing game

---
+++ toss_coin.py
@@ -0,0 +1,19 @@
+# Coin Toss
+
+import random
+
+guess = ''
+while guess not in (int(1), int(0)):
+    print('Guess the coin toss! Enter 1 for heads or 0 for tails:')
+    guess = int(input()) #Turned string into integer
+
+toss  = random.randint(0, 1) # 0 is tails, 1 is heads
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = int(input()) 
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You loose')
