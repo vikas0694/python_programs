fa3fa07ad6ddf3f15e2e33c8fceac0820931c6b1
vikas.0694 <vikas.0694@gmail.com>
2018-11-14 13:08:06 +0530

    coin flip

--- toss_coin.py
+++ toss_coin.py
@@ -6,7 +6,7 @@ while guess not in (int(1), int(0)):
     print('Guess the coin toss! Enter 1 for heads or 0 for tails:')
     guess = int(input()) 
 
-toss  = random.randint(0, 1) # 0 is tails, 1 is heads
+toss  = random.randint(0, 1)  # 0 is tails, 1 is heads
 if toss == guess:
     print('You got it!')
 else:
