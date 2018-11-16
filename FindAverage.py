4b937a8423ad334d458a173f8b2d0caa11f838be
vikas.0694 <vikas.0694@gmail.com>
2018-11-16 13:39:06 +0530

    find average with lambda function

---
+++ FindAverage.py
@@ -0,0 +1,11 @@
+# def average_finder(l1,l2):
+#     average = []
+#     for pair in zip(l1,l2):
+#         average.append(sum(pair)/len(pair))
+#     return average
+# print(average_finder([1,2,3],[4,5,6]))
+
+# with lambda function
+
+average_finder = lambda *args : [sum(pair)/len(pair) for pair in zip(*args)]
+print(average_finder([1,2,3],[4,5,6]))
